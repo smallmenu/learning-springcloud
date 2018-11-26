2b6f94c36ef52280ead57ae76170ee6e28b6cf1e
niuchaoqun <smallmenu@gmail.com>
2018-11-26 18:51:11 +0800

    * init

--- README.md
+++
@@ -1,2 +0,0 @@
-# learning-springcloud
-learning springcloud
---
+++ eureka-client/src/main/java/com/niuchaoqun/springcloud/eureka/controller/IndexController.java
@@ -0,0 +1,13 @@
+package com.niuchaoqun.springcloud.eureka.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+    @RequestMapping("/hello")
+    public String index(@RequestParam String name) {
+        return "hello "+name+"，this is first messge";
+    }
+}
