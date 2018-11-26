267bae47b507be70ce00b7025cbf25ee55e47066
niuchaoqun <niuchaoqun@coowin.co>
2018-11-26 23:13:24 +0800

    * update

--- eureka-client/src/main/java/com/niuchaoqun/springcloud/eureka/controller/IndexController.java
+++ eureka-client/src/main/java/com/niuchaoqun/springcloud/eureka/controller/IndexController.java
@@ -1,13 +1,27 @@
 package com.niuchaoqun.springcloud.eureka.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class IndexController {
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @RequestMapping("/instances/{applicationName}")
+    @ResponseBody
+    public List<ServiceInstance> serviceInstancesByApplicationName(
+            @PathVariable String applicationName) {
+        return this.discoveryClient.getInstances(applicationName);
+    }
+
     @RequestMapping("/hello")
     public String index(@RequestParam String name) {
-        return "hello "+name+"，this is first messge";
+        return "hello " + name + "，this is first messge";
     }
 }
