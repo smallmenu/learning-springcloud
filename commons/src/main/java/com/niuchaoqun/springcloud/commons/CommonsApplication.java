369d81aec90d582c0c9324f0c74bb85f6425928d
niuchaoqun <niuchaoqun@coowin.co>
2018-12-10 22:13:37 +0800

    * 修改增加 commons 模块

--- commons/src/main/java/com/niuchaoqun/springcloud/commons/CommonsApplication.java
+++ commons/src/main/java/com/niuchaoqun/springcloud/commons/CommonsApplication.java
@@ -1,7 +1,9 @@
 package com.niuchaoqun.springcloud.commons;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class CommonsApplication {
 
     public static void main(String[] args) {
